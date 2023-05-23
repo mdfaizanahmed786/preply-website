79536edf6a957a12e14f2832ebacce5b903612c5
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-05-23 13:58:16 +0530

    ui: added schedule section v2

--- components/MainBody/Schedule/Schedule.tsx
+++ components/MainBody/Schedule/Schedule.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoMdArrowDropdown,
+} from "react-icons/io";
 
 interface Schedule {}
 
@@ -9,7 +14,7 @@ const Schedule: React.FC<Schedule> = () => {
       <div>
         <p className="font-bold">Schedule</p>
       </div>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-6">
         <div className="bg-gray-100 p-2 flex gap-2 items-center">
           <AiOutlineQuestionCircle className="h-3 w-3 text-gray-600" />
           <p className="text-gray-600 text-sm">
@@ -17,7 +22,27 @@ const Schedule: React.FC<Schedule> = () => {
             your local timezone.
           </p>
         </div>
-        <div></div>
+
+        <div className="flex items-center ">
+          <div className="flex items-center gap-3 flex-1">
+            <div className="flex items-center gap-[1px]">
+              <div className="flex h-9 w-9 items-center justify-center shrink-0 grow-0  ring-1 ring-gray-300">
+                <IoIosArrowBack className="h-6 w-6 text-gray-300 cursor-not-allowed" />
+              </div>
+              <div className="h-9 w-9 flex items-center justify-center shrink-0 grow-0 bg-gray-100 ring-1 ring-gray-400">
+                <IoIosArrowForward className="text-gray-400 h-6 w-6 hover:text-primary cursor-pointer" />
+              </div>
+            </div>
+            <div>May 23–29, 2023</div>
+          </div>
+
+          
+            <div className="flex ring-gray-300 px-4 cursor-pointer py-1 ring-1 items-center gap-2">
+              <p className="flex-1 text-gray-400">Asia/Damascus GMT +3:00</p>
+              <IoMdArrowDropdown className="text-gray-400 h-4 w-4" />
+            </div>
+        
+        </div>
         <div></div>
         <div></div>
       </div>
